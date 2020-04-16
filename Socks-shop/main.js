647fa7c5bfd78db3894cbfc95162446f91e16eb9
Pufke <pufke1998@gmail.com>
2020-04-16 20:21:03 +0200

    Components

--- Socks-shop/main.js
+++ Socks-shop/main.js
@@ -1,6 +1,68 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+//Create a new component for product-details with a prop of details. 
+
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
+
+
+
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+   <div class="product">
+        
+      <div class="product-image">
+        <img :src="image" />
+      </div>
+
+      <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <product-details :details="details"></product-details>
+
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
+
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+
+          <div class="cart">
+            <p>Cart({{ cart }})</p>
+          </div>
+
+       </div>  
+    
+    </div>
+   `,
+  data() {
+    return {
         product: 'Socks',
         brand: 'Vue Mastery',
         selectedVariant: 0,
@@ -9,7 +71,7 @@ var app = new Vue({
           {
             variantId: 2234,
             variantColor: 'green',
-            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
             variantQuantity: 10     
           },
           {
@@ -19,16 +81,16 @@ var app = new Vue({
             variantQuantity: 0     
           }
         ],
-        cart: 0,
-        onSale: true
-    },
+        cart: 0
+    }
+  },
     methods: {
-        addToCart: function() {
-            this.cart += 1
-        },
-        updateProduct: function(index) {  
-            this.selectedVariant = index
-        }
+      addToCart: function() {
+          this.cart += 1
+      },
+      updateProduct: function(index) {  
+          this.selectedVariant = index
+      }
     },
     computed: {
         title() {
@@ -40,11 +102,18 @@ var app = new Vue({
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
         },
-        sale() {
-          if (this.onSale) {
-            return this.brand + ' ' + this.product + ' are on sale!'
-          } 
-            return  this.brand + ' ' + this.product + ' are not on sale'
+        shipping() {
+          if (this.premium) {
+            return "Free"
+          }
+            return 2.99
         }
     }
-  })
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      premium: true
+    }
+})
