ec06b5fb40c11fe739f331f031944fb860a44126
Pufke <pufke1998@gmail.com>
2020-04-23 17:32:56 +0200

    Data & Methods

--- Vue-JS-NetNinja/app.js
+++ Vue-JS-NetNinja/app.js
@@ -1,6 +1,13 @@
 new Vue({
     el: '#vue-app', //# because it is id of div
     data: {
-        name: 'Shuan'
+        name: 'Nemanja',
+        job : 'Programer'
+    },
+    methods: {
+        greet: function(time){
+           
+            return 'Good' + ' ' + time + ' ' + this.name;
+        }
     }
 });
