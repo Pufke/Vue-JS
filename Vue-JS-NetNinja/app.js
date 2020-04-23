20aa86cbafd0a958cf42d8925a9ebee03c0f5dab
Pufke <pufke1998@gmail.com>
2020-04-23 17:43:37 +0200

    Data Binding

--- Vue-JS-NetNinja/app.js
+++ Vue-JS-NetNinja/app.js
@@ -2,11 +2,12 @@ new Vue({
     el: '#vue-app', //# because it is id of div
     data: {
         name: 'Nemanja',
-        job : 'Programer'
+        job : 'Programer',
+        website: 'http://www.thenetninja.co.uk',
+        websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja</a>'
     },
     methods: {
         greet: function(time){
-           
             return 'Good' + ' ' + time + ' ' + this.name;
         }
     }
