76e982a0b4ef30aff97c72cbf1697ce6ceb5b8f4
Pufke <pufke1998@gmail.com>
2020-04-26 17:09:17 +0200

    Axios crash

---
+++ Vue-JS-Lessons/7. Looping with v-for/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#vue-app', //# because it is id of div
+    data: {
+        characters:['Nemanja','Vlaksi','Mario','Dario'],
+        ninjas:[
+            {name: 'Pera', age:23},
+            {name: 'Mile', age:22},
+            {name: 'Joca', age:35},
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+        
+    }
+});
---
+++ Vue-JS-Lessons/8. Refs/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#vue-app', //# because it is id of div
+    data: {
+        
+    },
+    methods: {
+
+    },
+    computed: {
+        
+    }
+});
